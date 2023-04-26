8ac6c96188383f14bd21b402184999c08f6987fa
thiago <thiago.alves@ifce.edu.br>
2023-04-26 19:59:57 -0300

    Mostra a maior venda entre os vendedores

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,22 @@ public class Main {
 
 		double valorVendas;
 		double totalVendasLoja = 0;
+		double maiorVenda = 0;
 
 		for (int i=0; i < quantidadeVendedores; i++){
 			System.out.printf("Digite o valor das vendas do %dº vendedor(a):%n", i+1);
 			valorVendas = input.nextDouble();
 			totalVendasLoja += valorVendas;
+
+			maiorVenda = valorVendas;
+
+			if (valorVendas > maiorVenda){
+				maiorVenda = valorVendas;
+			}
 		}
 
 		System.out.printf("Valor total de vendas da loja: %.2f%n", totalVendasLoja);
-		System.out.printf("Média de vendas dos vendedores: %.2f", totalVendasLoja/quantidadeVendedores);
+		System.out.printf("Média de vendas dos vendedores: %.2f%n", totalVendasLoja/quantidadeVendedores);
+		System.out.printf("Maior valor de vendas entre os vendedores: %.2f", maiorVenda);
 	}
 }
