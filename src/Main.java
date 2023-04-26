5b61154158390f17a89f2448e454574c31a4c146
thiago <thiago.alves@ifce.edu.br>
2023-04-26 20:09:49 -0300

    Mostra o número do vendedor que mais vendeu

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 a média de vendas de todos os vendedores. O usuário do
 programa deve fornecer a quantidade de vendedores da loja e o
 valor das vendas de cada vendedor.
+
+Modifique o programa anterior para mostrar o maior valor de
+vendas e o número do vendedor que mais vendeu.
 */
 
 
@@ -18,6 +21,7 @@ public class Main {
 		double valorVendas;
 		double totalVendasLoja = 0;
 		double maiorVenda = 0;
+		int melhorVendedor = 0;
 
 		for (int i=0; i < quantidadeVendedores; i++){
 			System.out.printf("Digite o valor das vendas do %dº vendedor(a):%n", i+1);
@@ -26,11 +30,13 @@ public class Main {
 
 			if (valorVendas > maiorVenda){
 				maiorVenda = valorVendas;
+				melhorVendedor = i + 1;
 			}
 		}
 
 		System.out.printf("Valor total de vendas da loja: %.2f%n", totalVendasLoja);
 		System.out.printf("Média de vendas dos vendedores: %.2f%n", totalVendasLoja/quantidadeVendedores);
-		System.out.printf("Maior valor de vendas entre os vendedores: %.2f", maiorVenda);
+		System.out.printf("Maior valor de vendas entre os vendedores: %.2f%n", maiorVenda);
+		System.out.printf("Vendedor que mais vendeu: %dº", melhorVendedor);
 	}
 }
